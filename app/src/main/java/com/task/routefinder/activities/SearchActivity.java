751dc67ef0d079fd921e81de8b635eecf1b04a7f
-v <manjitborah2710@gmail.com>
2019-06-22 00:19:56 +0530

    added current location feature

--- app/src/main/java/com/task/routefinder/activities/SearchActivity.java
+++ app/src/main/java/com/task/routefinder/activities/SearchActivity.java
@@ -1,32 +1,42 @@
 package com.task.routefinder.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ListView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.task.routefinder.R;
 import com.task.routefinder.adapters.AddLocationRecyclerViewAdapter;
-import com.task.routefinder.adapters.PlaceSearchResultsAdapter;
-import com.task.routefinder.models.PlaceInfo;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class SearchActivity extends AppCompatActivity{
+public class SearchActivity extends AppCompatActivity implements LocationListener{
 
     RecyclerView placesToBeSearched;
     AddLocationRecyclerViewAdapter adapter;
     FloatingActionButton addMoreLocationsBtn,doneBtn;
+    AutoCompleteTextView sourceEt;
 
 
     @Override
@@ -35,6 +45,7 @@ public class SearchActivity extends AppCompatActivity{
         setContentView(R.layout.activity_search);
         addMoreLocationsBtn=findViewById(R.id.add_location_fab_SearchActivity);
         doneBtn=findViewById(R.id.done_fab_SearchActivity);
+        sourceEt=findViewById(R.id.source_et_SearchActivity);
 
 
 
@@ -72,7 +83,7 @@ public class SearchActivity extends AppCompatActivity{
 
                 List<String> allData = adapter.getAllData();
 //                Log.d("mn",allData.toString()+"\n"+allData.isEmpty());
-                if(allData.size()>=2) {
+                if(allData.size()>=1) {
                     Intent intent = new Intent();
                     intent.putExtra("result", allData.toArray(new String[adapter.getAllData().size()]));
                     setResult(RESULT_OK, intent);
@@ -85,18 +96,71 @@ public class SearchActivity extends AppCompatActivity{
             }
         });
 
-
-
+        getCurrentLocation();
 
 
 
     }
 
-    public boolean checkForPlaceAvailabiltiy(String place){
-        if(place.equalsIgnoreCase("Silchar")){
-            return true;
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1001 && grantResults.length!=0){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }
         }
-        return false;
     }
 
+    private void getCurrentLocation(){
+        List<String> permissions=new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            permissions.clear();
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if(!permissions.isEmpty()){
+            ActivityCompat.requestPermissions(this,permissions.toArray(new String[permissions.size()]),1001);
+        }
+        else{
+            LocationManager locationManager= (LocationManager) getSystemService(LOCATION_SERVICE);
+            if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,100000000,0,this);
+            }
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("mn",location.toString());
+        LatLng latLng=new LatLng(location.getLatitude(),location.getLongitude());
+        Geocoder geocoder=new Geocoder(this);
+        try{
+            List<Address> addresses=geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+            sourceEt.setText(addresses.get(0).getAddressLine(0));
+        }
+        catch (IOException e){
+            Log.d("mn","io "+e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
